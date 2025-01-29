88ae7a9d564c9599ffa308df7eeb32b1848931a3
Ana Karina <116378764+AnadevopsCaleidos@users.noreply.github.com>
2025-01-29 17:45:29 -0500

    Synchronizing job segundo_job from AWS Glue at 2025/01/29 22:45:29

---
+++ segundo_job/segundo_job.py
@@ -0,0 +1,24 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+
+input_path = "s3://s3://version-control-glue-test/segundo-job.csv"  
+df = spark.read.format("csv").option("header", "true").load(input_path)  
+
+#Prueba de conexion a github
+#Prueba de pipeline conexion 
+df.show()
+
+
+job.commit()
